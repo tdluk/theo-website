8fdcf6a14e8b44db979b3d03eac9456668ef8849
Theo Lukens <theo@theos-macbook.local>
2021-03-02 23:30:07 +0000

    Actually add the files this time...

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "theo-website",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/src/data`,
+      },
+    },
+    "gatsby-transformer-remark"
+  ],
 };
